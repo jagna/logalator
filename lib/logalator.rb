74386c7e64702a0ac0401bcbc5b907b1adff9c21
jagna <agnieszkalewandowska+github@gmail.com>
2010-01-31 23:11:27 +0100

    Parsing access logs files.

---
+++ lib/logalator.rb
@@ -0,0 +1,41 @@
+require 'rubygems'
+require 'dm-core'
+require 'model/request'
+
+DataMapper.setup(:default, 'postgres://ARGV[1]:ARGV[2]@localhost/logizator')
+DataMapper.auto_upgrade!
+
+if ARGV[0].nil? || !File.directory?(ARGV[0])
+    puts "Give dir as an argument"
+    exit  
+end
+
+line_re = /^(([\d\.])+)(\s\S+){2}\s\[(.*?)\]\s"((\S+)\s(\S+)\s(\S+))"\s(\d+|-)\s(\d+|-)\s"(.*?)"\s"(.+?)"$/
+@robots = []
+File.open('ignored_agents.txt').each_line { |r| @robots << Regexp.new(r.strip) unless "#" == r[0]}
+
+def robot?(agent)
+    @robots.any { |robot| agent =~ robot }
+end
+
+done = File.open(File.join(ARGV[0], 'done_logs.txt'), "w+")
+Dir.foreach(ARGV[0]) do |f|
+   path = File.join(ARGV[0], f)
+   next if !File.file?(path) && File.directory?(path) 
+   i=0
+   File.open(path, "r").each do |l|
+       i = i+1
+       el = l.match(line_re)
+       time = DateTime::parse("#{el[4]}".sub(/:/, ' ')) 
+       u = User.first_or_create( :ip => el[1],:agent => el[12], :robot => robot?(el[12]) )
+       h = Header.first_or_create( :method => el[6], :path => el[7], :protocol => el[8], :status => el[9], :bytes => el[10] )
+       r = Request.create( :nr => i, :referrer => el[11], :date => time, :user => u, :header => h)
+       if i % 25000 == 0
+           print "\r#{f} #{i}"
+           STDOUT.flush
+       end
+   end
+   puts
+   done.puts("#{path} lines: #{i}")
+end
+done.close
---
+++ lib/model/request.rb
@@ -0,0 +1,35 @@
+class User
+    include DataMapper::Resource
+
+    has n, :requests
+
+    property :id, Serial
+    property :ip, String
+    property :agent, String, :length => 4000
+    property :robot, Boolean
+end
+
+class Header
+    include DataMapper::Resource
+
+    has n, :requests
+
+    property :id, Serial
+    property :method, String, :index => true
+    property :path, String, :index => true, :length => 4000 
+    property :protocol, String
+    property :status, String, :index => true
+    property :bytes, String 
+end
+
+class Request
+    include DataMapper::Resource
+
+    belongs_to :header
+    belongs_to :user
+
+    property :id, Serial
+    property :referrer, String, :length => 4000
+    property :date, DateTime, :index => true
+    property :nr, Integer
+end
