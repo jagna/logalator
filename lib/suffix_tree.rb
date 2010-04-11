1e35a0a0c7c8e516d94362b588fbc105e6c9e860
jagna <agnieszkalewandowska+github@gmail.com>
2010-04-11 18:45:30 +0200

    Added suffix tree.

---
+++ lib/suffix_tree.rb
@@ -0,0 +1,109 @@
+# 
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+ 
+require 'pp'
+
+    class SuffixTreeBuilder
+      attr_reader :sf 
+      
+      def initialize
+        @sf = SuffixTree.new
+      end
+      
+      def add_tree tree, path=[]
+        tree.each do |k,v| 
+          path << k.request.page.name.to_sym; 
+          if v.empty?
+            self << path if path.size > 1
+          else
+            add_tree v, path
+          end
+        end
+      end
+      
+      def <<(session_pages)
+        (session_pages.size-1).times do
+          @sf << session_pages
+          session_pages.shift
+        end
+        self        
+      end
+      
+      def to_s
+        "suffix tree build:\n#{@sf.to_s}"
+      end
+
+      def to_s_sort
+        @sf.to_s_sort
+      end
+    end
+    
+    class SuffixTree
+      
+      def initialize
+        @sf = {}
+      end
+      
+      def <<(pages)        
+        current = @sf        
+        pages.each do |page|  
+          current[page] ||= {}          
+          current = current[page]
+          
+          current[:leaf] ||= 0
+          current[:leaf] += 1
+        end
+        self
+      end
+      
+      def to_s
+        sf_string = ''
+        letter(sf_string, [], @sf)       
+        sf_string
+      end
+
+      def to_s_sort
+          count_path = {}
+          substring(count_path, [], @sf)
+          count_path.sort { |a,b| if b[1].eql? a[1] then b[0].size <=> a[0].size else b[1] <=> a[1] end }.each do |k,v|
+              puts "count: #{v} path: #{k}"
+          end
+      end
+      
+      private 
+      def substring(count_path, path, hash_map)
+        hash_map.each_key do |key|
+          if(:leaf == key)
+              count_path["#{path.map {|i| i.to_s}}"] = hash_map[:leaf] if path.size > 1 + 1 && hash_map[:leaf] > 10
+          else     
+            path << key
+            substring(count_path, path, hash_map[key]) 
+            path.pop
+          end          
+        end
+      end
+
+      def letter(sf_string, path, hash_map)
+        hash_map.each_key do |key|
+          if(:leaf == key)
+            sf_string << "\tpath: #{path.map {|i| i.to_s}} with count #{hash_map[:leaf]}\r\n" if path.size > 1 + 1 && hash_map[:leaf] > 10
+          else     
+            path << key
+            letter(sf_string, path, hash_map[key]) 
+            path.pop
+          end          
+        end
+      end
+    end
+=begin
+   stb = SuffixTreeBuilder.new
+   1.times do
+    stb << %w(t a t a t t a t a t a)
+    stb << %w(b a n a n a s)    
+    stb << %w(k a k a j)
+    end
+
+   puts stb
+=end
+
---
+++ lib/treebuilder.rb
@@ -0,0 +1,48 @@
+require 'rubygems'
+require 'dm-core'
+require 'dm-aggregates'
+require 'logalator/model/request'
+require 'logalator/model/session'
+require 'logalator/progressbar'
+require 'suffix_tree'
+
+DataMapper::Logger.new(STDOUT, :info)
+DataMapper.setup(:default, "postgres://#{ARGV[0]}:#{ARGV[1]}@localhost/#{ARGV[2]}")
+DataMapper.auto_upgrade!
+
+def shring_content
+
+end
+
+si_co = SessionItem.count
+puts "Session items #{si_co}"
+DIVIDER = 10000
+START=-1
+LEAF=-2
+CONTENT = "Content"
+@pb = Console::ProgressBar.new("SessionItems", si_co/DIVIDER)
+i=0
+@stb = SuffixTreeBuilder.new
+Session.all.each do |s|
+    c_paths = {}#key - last element of path, value whole path 
+    paths = []
+    SessionItem.all(:session_id => s.id, :order => [:date, :nr]).each do |si|
+        @pb.inc if (i+=1) % DIVIDER == 0
+        if(si.session_item_id) #I as child
+            c_paths[si.id] = []
+            c_paths[si.id].replace (c_paths[si.session_item_id]) 
+            c_paths[si.id] << si.page.name unless c_paths[si.id].last.eql?(CONTENT) && si.page.name.eql?(CONTENT)
+        else #I as head
+            c_paths[si.id] = []
+            c_paths[si.id] << START
+            c_paths[si.id] << si.page.name
+        end
+        if 0 == SessionItem.count(:session_id => s.id, :session_item_id => si.id)#I as a leaf
+            c_paths[si.id] << LEAF
+            paths << c_paths[si.id] 
+        end
+    end
+    paths.each { |l| @stb << l; }
+end
+@stb.to_s_sort
+@pb.finish
